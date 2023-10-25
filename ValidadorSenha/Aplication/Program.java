13bc4ac7e5e7292d7a512c0078942df4a8bf20da
Kenia de Oliveira <keniapubg@gmail.com>
2023-10-25 11:16:24 -0300

    Adicionando o validador de senhas

---
+++ ValidadorSenha/Aplication/Program.java
@@ -0,0 +1,51 @@
+package org.Learnig.ValidadorSenha.Aplication;
+
+import org.Learnig.ValidadorSenha.entities.Validacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        Validacao validacao = new Validacao();
+
+
+        boolean senhaForte = false;
+
+        while (!senhaForte) {
+            System.out.print("\nDigite sua senha: \n");
+            validacao.senha = scanner.nextLine();
+            if (validacao.maiusculas() != null || validacao.minusculas() != null ||
+                validacao.especiais() != null || validacao.numero() != null || validacao.tamanho() != null)
+            {
+                System.out.println("SENHA MUITO FRACA");
+                if (validacao.maiusculas() != null) {
+                    System.out.println(validacao.maiusculas());
+                }
+
+                if (validacao.minusculas() != null) {
+                    System.out.println(validacao.minusculas());
+                }
+
+                if (validacao.especiais() != null) {
+                    System.out.println(validacao.especiais());
+                }
+
+                if (validacao.numero() != null) {
+                    System.out.println(validacao.numero());
+                }
+
+                if (validacao.tamanho() != null) {
+                    System.out.println(validacao.tamanho());
+                }
+            }
+            else {
+                senhaForte = true;
+                System.out.println("SENHA FORTE!");
+            }
+        }
+    }
+
+}
---
+++ ValidadorSenha/entities/Validacao.java
@@ -0,0 +1,44 @@
+package org.Learnig.ValidadorSenha.entities;
+
+public class Validacao {
+    public String senha;
+    public static final String MAIUSCULAS = ".*[A-Z].*";
+    public static final String MINUSCULAS = ".*[a-z].*";
+    public static final String ESPECIAIS = ".*[.\\-\\[\\]*~_@#:?].*";
+    public static final String NUMERO = ".*\\d.*";
+
+    public String maiusculas(){
+        if (!senha.matches(".*[A-Z].*")){
+            return "A senha precisa ter pelo menos uma letra maiuscula!";
+        }
+        return null;
+    }
+
+    public String minusculas() {
+        if (!senha.matches(".*[a-z].*")) {
+            return "A senha precisa ter pelo menos uma letra minuscula!";
+        }
+        return null;
+    }
+
+    public String especiais() {
+        if(!senha.matches(".*[.\\-\\[\\]*~_@#:?].*")) {
+            return "A senha precisa ter pelo menos um caractere especial!";
+        }
+        return null;
+    }
+
+    public String numero() {
+        if (!senha.matches(".*\\d.*")) {
+            return "A senha precisa ter pelo menos um numero!";
+        }
+        return null;
+    }
+
+    public String tamanho() {
+        if (senha.length() < 8) {
+            return "Senha muito pequena";
+        }
+        return null;
+    }
+}
