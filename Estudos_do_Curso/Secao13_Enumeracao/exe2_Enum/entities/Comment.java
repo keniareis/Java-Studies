70d05a32520ae0712a49523db9f75b6aba2e4aca
Kenia de Oliveira <keniapubg@gmail.com>
2023-12-13 21:00:40 -0300

    Segundo exercicio envolvendo enums

---
+++ Estudos_do_Curso/Secao13_Enumeracao/exe2_Enum/entities/Comment.java
@@ -0,0 +1,21 @@
+package org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe2_Enum.entities;
+
+public class Comment {
+
+    private String text;
+
+    public Comment(){
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
