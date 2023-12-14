f38630ea17ec35e3ceaa098d17765f2117c4b7fd
Kenia de Oliveira <keniapubg@gmail.com>
2023-12-14 08:19:12 -0300

    Quase terminando as classes do entities e iniciando o Program

--- Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/application/Program.java
+++ Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/application/Program.java
@@ -1,4 +1,15 @@
 package org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe_Fixacao.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+        scanner.close();
+    }
 }
--- Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/entities/Order.java
+++ Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/entities/Order.java
@@ -1,4 +1,67 @@
 package org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe_Fixacao.entities;
 
+import org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe_Fixacao.entities.enums.OrderStatus;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Order {
+
+    private Date moment;
+    private OrderStatus status;
+    private Client client;
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public Order(){
+    }
+
+    public Order(Date moment, OrderStatus status) {
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Client getClients() {
+        return client;
+    }
+    public void setClients(Client client) {
+        this.client = client;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void addItem(OrderItem item){
+        orderItems.add(item);
+    }
+    public void removeItem(OrderItem item){
+        orderItems.remove(item);
+    }
+
+    public double total(){
+        double sum = 0.0;
+        for (OrderItem item: items) {
+            sum += item.subTotal();
+        }
+        return sum;
+    }
+
 }
