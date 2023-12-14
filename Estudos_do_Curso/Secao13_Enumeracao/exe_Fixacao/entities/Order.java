dfafd6a7781dd16dac75f45f276b89414a33f47d
Kenia de Oliveira <keniapubg@gmail.com>
2023-12-14 19:25:35 -0300

    Quase terminando o exe de fixacao de enums

--- Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/entities/Order.java
+++ Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/entities/Order.java
@@ -1,25 +1,30 @@
 package org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe_Fixacao.entities;
 
-import org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe_Fixacao.entities.enums.OrderStatus;
+import org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.entities.enums.OrderStatus;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+
 public class Order {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
     private Date moment;
     private OrderStatus status;
+
     private Client client;
-    private List<OrderItem> orderItems = new ArrayList<>();
+    private List<OrderItem> items = new ArrayList<>();
 
-    public Order(){
-    }
-
-    public Order(Date moment, OrderStatus status) {
+    public Order(Date moment, OrderStatus status, Client client) {
         this.moment = moment;
         this.status = status;
         this.client = client;
+    }
+
+    public Order(Date moment, org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe_Fixacao.entities.enums.OrderStatus status, Client client) {
     }
 
     public Date getMoment() {
@@ -38,30 +43,49 @@ public class Order {
         this.status = status;
     }
 
-    public Client getClients() {
+    public Client getClient() {
         return client;
     }
-    public void setClients(Client client) {
+
+    public void setClient(Client client) {
         this.client = client;
     }
 
-    public List<OrderItem> getOrderItems() {
-        return orderItems;
+    public List<OrderItem> getItems() {
+        return items;
     }
 
-    public void addItem(OrderItem item){
-        orderItems.add(item);
-    }
-    public void removeItem(OrderItem item){
-        orderItems.remove(item);
+    public void addItem(OrderItem item) {
+        items.add(item);
     }
 
-    public double total(){
+    public void removeItem(OrderItem item) {
+        items.remove(item);
+    }
+
+    public double total() {
         double sum = 0.0;
-        for (OrderItem item: items) {
+        for (OrderItem item : items) {
             sum += item.subTotal();
         }
         return sum;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order moment: ");
+        sb.append(sdf.format(moment) + "\n");
+        sb.append("Order status: ");
+        sb.append(status + "\n");
+        sb.append("Client: ");
+        sb.append(client + "\n");
+        sb.append("Order items:\n");
+        for (OrderItem item : items) {
+            sb.append(item + "\n");
+        }
+        sb.append("Total price: $");
+        sb.append(String.format("%.2f", total()));
+        return sb.toString();
+    }
 }
--- Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/entities/OrderItem.java
+++ Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/entities/OrderItem.java
@@ -11,7 +11,7 @@ public class OrderItem {
     public OrderItem(){
     }
 
-    public OrderItem(Integer quantity, Double price) {
+    public OrderItem(Integer quantity, Double price, Product product) {
         this.quantity = quantity;
         this.price = price;
     }
