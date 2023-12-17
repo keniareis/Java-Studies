46622ee8dd82254e2b457a8a13d7bde2496b4698
Kenia de Oliveira <keniapubg@gmail.com>
2023-12-17 17:23:12 -0300

    Refazendo o exe de Fixacao

---
+++ Estudos_do_Curso/Secao13_Enumeracao/exeFixacao/entities/Order.java
@@ -0,0 +1,65 @@
+package org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exeFixacao.entities;
+
+import org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe_Fixacao.entities.enums.OrderStatus;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+    private Date moment;
+    private OrderStatus status;
+    private Client client;
+    private List<OrderItem> items = new ArrayList<>();
+
+
+    public Order(Date moment, OrderStatus status, Client client) {
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void addItem(OrderItem item){
+        items.add(item);
+    }
+    public void removeItem(OrderItem item){
+        items.remove(item);
+    }
+
+    public Double total(){
+        double sum = 0.0;
+        for (OrderItem item:items) {
+            sum += item.subTotal();
+        }
+        return sum;
+    }
+
+}
