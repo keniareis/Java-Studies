1015a4460a45556ddad1f074d286ec0da3878691
Kenia de Oliveira <keniapubg@gmail.com>
2023-12-18 21:44:21 -0300

    Excluindo as tentativas falhas

--- Estudos_do_Curso/Secao13_Enumeracao/exe_Fixacao/entities/enums/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package org.Learnig.Estudos_do_Curso.Secao13_Enumeracao.exe_Fixacao.entities.enums;
-
-public enum OrderStatus {
-    PENDING_PAYMENT,
-    PROCESSING,
-    SHIPPED,
-    DELIVERED;
-}
