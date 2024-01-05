591bbc32d53895794c3a0d6b3a9b0b85c774d413
Kenia de Oliveira <keniapubg@gmail.com>
2024-01-05 20:42:39 -0300

    treinando uma logica basica

---
+++ Logica/Manzano/Cap_06.java
@@ -0,0 +1,55 @@
+package org.Learnig.Logica.Manzano;
+
+import java.util.*;
+
+public class Cap_06 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        //--A--
+//        String[] nomes = new String[10];
+//        for (int i = 0; i < nomes.length; i++) {
+//            nomes[i] = scanner.nextLine();
+//        }
+//        System.out.println(Arrays.toString(nomes));
+//
+//
+//        //--B--
+//        int[] A = new int[8];
+//        int[] B = new int[8];
+//        for (int i = 0; i < A.length; i++) {
+//            A[i] = scanner.nextInt();
+//            B[i] = (A[i] * 3);
+//        }
+//        System.out.println(Arrays.toString(B));
+//
+//
+//        //--C--
+//        double[] A = new double[20];
+//        double[] B = new double[20];
+//        double[] C = new double[20];
+//
+//        for (int i = 0; i < A.length; i++) {
+//            A[i] = random.nextDouble() * 10;
+//            B[i] = random.nextDouble() * 10;
+//            C[i] = A[i] - B[i];
+//        }
+//        System.out.println(Arrays.toString(C));
+//
+
+        //--D--
+        int[] A = new int[15];
+        int[] B = new int[15];
+        for (int i = 0; i < A.length; i++) {
+            A[i] = random.nextInt(10);
+            B[i] = (int) Math.pow(A[i], 2);
+        }
+        System.out.println(Arrays.toString(A));
+        System.out.println(Arrays.toString(B));
+
+
+        scanner.close();
+    }
+}
