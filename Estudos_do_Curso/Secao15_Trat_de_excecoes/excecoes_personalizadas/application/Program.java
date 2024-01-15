155e8fe28b453539cae401fe3c2f39726eb091ad
Kenia de Oliveira <keniapubg@gmail.com>
2024-01-15 18:05:52 -0300

    agora a solução 'ruim'

--- Estudos_do_Curso/Secao15_Trat_de_excecoes/excecoes_personalizadas/application/Program.java
+++ Estudos_do_Curso/Secao15_Trat_de_excecoes/excecoes_personalizadas/application/Program.java
@@ -37,13 +37,11 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkout = sdf.parse(scanner.next());
 
-            Date now = new Date();
-            if (checkin.before(now) || checkout.before(now)) {
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            }else if (!checkout.after(checkin)){
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            String error = reservation.updateDates(checkin, checkout);
+
+            if (error != null) {
+                System.out.println("Error in Reservation: " + error);
             }else {
-                reservation.updateDates(checkin, checkout);
                 System.out.println("Reservation: " + reservation);
             }
 
--- Estudos_do_Curso/Secao15_Trat_de_excecoes/excecoes_personalizadas/model/entities/Reservation.java
+++ Estudos_do_Curso/Secao15_Trat_de_excecoes/excecoes_personalizadas/model/entities/Reservation.java
@@ -38,9 +38,18 @@ public class Reservation {
         long diff = checkout.getTime() - checkin.getTime();
         return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
     }
-    public void updateDates(Date checkin, Date checkout){
+    public String updateDates(Date checkin, Date checkout){
+
+        Date now = new Date();
+        if (checkin.before(now) || checkout.before(now)) {
+            return "Error in reservation: Reservation dates for update must be future dates";
+        }
+        if (!checkout.after(checkin)) {
+            return "Error in reservation: Check-out date must be after check-in date";
+        }
         this.checkin = checkin;
         this.checkout = checkout;
+        return null;
     }
 
     @Override
