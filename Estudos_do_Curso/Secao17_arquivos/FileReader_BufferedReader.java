b02f3c31c611f3084517eb1bb7cdcf6a3ff3ac08
Kenia de Oliveira <keniapubg@gmail.com>
2024-01-28 20:13:30 -0300

    filereader e bufferedreader de uma forma melhor

--- Estudos_do_Curso/Secao17_arquivos/FileReader_BufferedReader.java
+++ Estudos_do_Curso/Secao17_arquivos/FileReader_BufferedReader.java
@@ -7,32 +7,17 @@ import java.io.IOException;
 public class FileReader_BufferedReader {
     public static void main(String[] args){
         String path = "C:\\Users\\KENIA\\IdeaProjects\\Forms\\src\\main\\java\\org\\Learnig\\Estudos_do_Curso\\Secao17_arquivos\\teste.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
 
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine();
 
             while (line != null){
                 System.out.println(line);
                 line = br.readLine();
             }
-        }catch (IOException e){
+        }catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        }finally {
-            try {
-                if (br != null){
-                    br.close();
-                }
-                if (fr != null){
-                    fr.close();
-                }
-            }catch (IOException e){
-                e.printStackTrace();
-            }
         }
     }
 }
