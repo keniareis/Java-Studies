1977d96181b1d347020488e14c54748778936185
Kenia de Oliveira <keniapubg@gmail.com>
2024-01-29 16:20:19 -0300

    estudando arquivos

---
+++ Estudos_do_Curso/Secao17_arquivos/FileWriter_BufferedWriter.java
@@ -0,0 +1,9 @@
+package org.Learnig.Estudos_do_Curso.Secao17_arquivos;
+
+public class FileWriter_BufferedWriter {
+    public static void main(String[] args){
+
+        String[] lines = new String[] {"Good morning", "Good afternoom"};
+
+    }
+}
