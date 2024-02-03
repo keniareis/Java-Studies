923d14feaff1d1f90b33080d1dc2dd1abf4310a8
keniareis <keniapubg@gmail.com>
2024-02-03 13:36:40 -0300

    fazendo um fatorial recursivo

--- Estruturas_de_Dados/Recursao/Recursao.java
+++ Estruturas_de_Dados/Recursao/Recursao.java
@@ -3,6 +3,7 @@ package org.Learnig.Estruturas_de_Dados.Recursao;
 public class Recursao {
     public static void main(String[] args){
         Recursiva(5);
+        System.out.println(fat(5));
     }
     public static int Recursiva(int i){
         System.out.println(i);
@@ -11,5 +12,11 @@ public class Recursao {
         else
             return Recursiva(i - 1);
 
+    }
+    public static int fat(int i){
+        if (i == 1)
+            return 1;
+        else
+            return i * fat(i - 1);
     }
 }
