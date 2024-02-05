d5c379f38a059bd83348ec17e94994dc9f25b552
keniareis <keniapubg@gmail.com>
2024-02-05 14:58:14 -0300

    fixando o quickSort

--- Estruturas_de_Dados/QuickSort/QuickSort.java
+++ Estruturas_de_Dados/QuickSort/QuickSort.java
@@ -15,30 +15,30 @@ public class QuickSort {
 
     public static void quickSort(int[] array, int menor, int maior){
         if (menor < maior){
-            int pivo = particao(array, menor, maior);
-            quickSort(array, menor, pivo - 1);
+            int pivo = particao(array, menor, maior);//chama o metodo partção para obter o indice do pivo
+            quickSort(array, menor, pivo - 1);//realiza a ordenação recursiva nas duas subpartiçoes
             quickSort(array, pivo + 1, maior);
         }
     }
 
-    static int particao(int[] array, int menor, int maior){
-        int pivo = array[menor];
-        int i = menor + 1;
+    public static int particao(int[] array, int menor, int maior){//escolher o pivo para a ordenação
+        int pivo = array[menor]; //selecionar o primeiro elemento como pivo
+        int i = menor + 1;//inicializar os indices para percorrer o array
         int j = maior;
 
-        while (true){
-            while (i < maior && array[i] < pivo) {i++;}
-            while (j > menor && array[j] > pivo){ j--;}
+        while (true){//será interrompido quando os indices 'i' e 'j' se cruzarem
+            while (i < maior && array[i] < pivo){i++;}
+            while (j > menor && array[j] > pivo){j--;}
 
             if (i >= j){
-                virar(array, menor, j);
+                virar(array, menor, j);//se 'i' e 'j' se cruzarem, troca o pivo para a posiçao correta e retorna o indice final do pivo
                 return j;
             }
             virar(array, i, j);
         }
     }
 
-    static void virar(int[] array, int i, int j){
+    public static void virar(int[] array, int i, int j){//troca os elementos dos indices 'i' e 'j' do array
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
