57229473d02d0e0833d4ac6f70cb44ce0f45ba10
keniareis <keniapubg@gmail.com>
2024-02-27 23:39:19 -0300

    fazendo uma arvore binaria em java

---
+++ Estruturas_de_Dados/ArvoreBinaria/Arvore.java
@@ -0,0 +1,9 @@
+package org.Learnig.Estruturas_de_Dados.ArvoreBinaria;
+
+public class Arvore<TIPO> {
+    private Elemento<TIPO> raiz;
+    public Arvore(){
+        this.raiz = null;
+
+    }
+}
---
+++ Estruturas_de_Dados/ArvoreBinaria/Elemento.java
@@ -0,0 +1,36 @@
+package org.Learnig.Estruturas_de_Dados.ArvoreBinaria;
+
+public class Elemento<TIPO> {
+    private TIPO valor;
+    private Elemento<TIPO> esquerda;
+    private Elemento<TIPO> direita;
+    public Elemento(TIPO valor, Elemento<TIPO> esquerda, Elemento<TIPO> direita){
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+
+    public TIPO getValor() {
+        return valor;
+    }
+
+    public void setValor(TIPO valor) {
+        this.valor = valor;
+    }
+
+    public Elemento<TIPO> getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(Elemento<TIPO> esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public Elemento<TIPO> getDireita() {
+        return direita;
+    }
+
+    public void setDireita(Elemento<TIPO> direita) {
+        this.direita = direita;
+    }
+}
